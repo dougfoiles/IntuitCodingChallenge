e0a798e261f184556a6b8de6d87193cc34263938
dougfoiles <dj.foiles@gmail.com>
2016-01-23 19:54:18 -0800

    Update CodingChallenge.java

--- CodingChallenge.java
+++ CodingChallenge.java
@@ -163,7 +163,7 @@ import java.lang.*;
             return true;
          }
          
-         for(int i = 2; i < (num/2); i++) {
+         for(int i = 2; i < num; i++) {
             if(num % i == 0) {
                 return false;  
             }
